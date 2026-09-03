bba8b41a8b37b885bf7fbacd6fa0aa2c2add0444
ruternix <arturkummer08@gmail.com>
2026-09-03 13:57:56 +0200

    some surface stuff

--- hosts/surface/configuration.nix
+++ hosts/surface/configuration.nix
@@ -8,12 +8,18 @@
 
   networking.hostName = "surface";
 
+  boot.kernelParams = [
+    "intel_pstate=hwp_dynamic_boost=0"
+    "pcie_aspm=force"
+  ];
+
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
   hardware.microsoft-surface.kernelVersion = "stable";
 
   hardware.sensor.iio.enable = true;
-
+  
+  services.power-profiles-daemon.enable = true;
   services.thermald.enable = true;
 }
