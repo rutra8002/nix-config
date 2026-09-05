2fa99cf1f63dc7f5be1d1835b4b5cc5787998621
ruternix <arturkummer08@gmail.com>
2026-09-05 09:32:57 +0200

    mst

--- home/common.nix
+++ home/common.nix
@@ -94,19 +94,18 @@
     enable = true;
     setAsDefaultBrowser = true;
 
-    policies = {
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          install_url =
-            "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
+    policies = let
+      mkExtensionSettings = builtins.mapAttrs (_: addonId: {
+        install_url =
+          "https://addons.mozilla.org/firefox/downloads/latest/${addonId}/latest.xpi";
+        installation_mode = "force_installed";
+      });
+    in {
+      ExtensionSettings = mkExtensionSettings {
+        "uBlock0@raymondhill.net" = "ublock-origin";
 
-        "446900e4-71c2-419f-a6a7-df9c091e268b" = {
-          install_url =
-            "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" =
+          "bitwarden-password-manager";
       };
     };
   };
